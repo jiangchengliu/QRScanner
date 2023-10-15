43233313d7d9a27fc0d306773046db0588287634
AsadSoomr0 <soomro.asad.2002@gmail.com>
2023-10-15 16:01:07 -0400

    Implementing unique QR codes for each user

--- QRScanner/app/models.py
+++ QRScanner/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
