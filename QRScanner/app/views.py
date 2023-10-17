e0a65f40e954432d5cf4efaf1c4c8fa854341c2b
Jiang Cheng Liu <jiangcl@bu.edu>
2023-10-17 12:34:10 -0400

    feat/chore: migrations and oauth stuff

--- QRScanner/app/views.py
+++ QRScanner/app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import qrcode
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+"""
+def generate_qr(request):
+    if request.method == 'POST':
+        data = request.POST['qr_data']
+        img = qrcode.make(data)
+        img.save('app/static/app/qr.png')
+        return render(request, 'app/index.html')
+    else:
+        return render(request, 'app/index.html')
+"""
+
+
+
+
