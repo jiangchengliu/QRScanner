a813a449a4d517d3f28d0278b0880d1462d947a9
AsadSoomr0 <soomro.asad.2002@gmail.com>
2023-10-18 16:42:06 -0400

    Added qrcode field to user model

--- QRScanner/app/models.py
+++ QRScanner/app/models.py
@@ -8,12 +8,11 @@ class CustomUser(AbstractUser):
     qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
 
     def __str__(self):
-        return self.email
-class QRCode(models.Model):
-user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='qr_code_user')
-qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
+        return self.email        class QRCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='qr_code_user')
+    qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
 
-def __str__(self):
+    def __str__(self):
     return self.user.email
 """
 
