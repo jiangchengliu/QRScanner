889761f081fd127c9a9deffeb2e0cfeb8e09c432
AsadSoomr0 <soomro.asad.2002@gmail.com>
2023-10-18 16:56:46 -0400

    customuser model

--- QRScanner/app/models.py
+++ QRScanner/app/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
 
 # Create your models here.
-"""
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
 
     def __str__(self):
-        return self.email        class QRCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='qr_code_user')
-    qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
+        return str(self.email)
+    
+    def save(self, *args, **kwargs):
+        qrcodeimg = qrcode.make(self.email)
+        canvas = Image.new('RGB', (qrcodeimg.pixel_size, qrcodeimg.pixel_size,), 'white')
+        draw = ImageDraw.draw(canvas)
+        canvas.paste(qrcodeimg)
+        fname = f'qr_code-{self.email.png}'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
 
-    def __str__(self):
-    return self.user.email
-"""
-
-class User(AbstractUser):
-    pass
 
 
     
