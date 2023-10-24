2d046e66fb16fa0243ac27eafe6a00b156acdeac
Rowan Quinn Whelan <rWhelan340@gmail.com>
2023-10-24 18:32:02 -0400

    feat:SheetApi

--- QRScanner/QRScanner/sheets_api.py
+++ QRScanner/QRScanner/sheets_api.py
@@ -1,24 +1,54 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials 
 import pprint
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import datetime
 
-#Authorize the API
-scope = [
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.file'
+
+
+
+# Set up the credentials
+credentials = service_account.Credentials.from_service_account_file(
+    'C:\Users\rwhel\OneDrive\Desktop\Notes\College\.CLUBS\Upe\QRScanner\client_key.json', 
+    scopes=['https://www.googleapis.com/auth/spreadsheets']
+)
+
+# Create a Google Sheets service
+service = build('sheets', 'v4', credentials=credentials)
+
+# Create a new Google Sheets document
+spreadsheet = service.spreadsheets().create(body={
+    'properties': {
+        'title': 'UPE_attendance_sheet'
+    }
+}).execute()
+
+# Extract the spreadsheet ID
+spreadsheet_id = spreadsheet['spreadsheetId']
+
+print(f"Created new Dub Sheet with ID: {spreadsheet_id}")
+
+title = datetime.datetime.now()
+
+# Create a request to add a new subsheet
+add_sheet_request = {
+    "requests": [
+        {
+            "addSheet": {
+                "properties": {
+                    "title": title
+                }
+            }
+        }
     ]
-file_name = 'client_key.json'
-creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
-client = gspread.authorize(creds)
+}
 
-#Fetch the sheet
-sheet = client.open('Tester').sheet1
-python_sheet = sheet.get_all_records()
-pp = pprint.PrettyPrinter()
-pp.pprint(python_sheet)
+# Execute the request to add the new subsheet
+response = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=add_sheet_request).execute()
 
-#Insert Row
-input = "what do you want to put in the sheets?"
-row = [input]
-index = 1
-sheet.insert_row(row,index)
+# Extract the ID of the newly created subsheet
+new_sheet_id = response['replies'][0]['addSheet']['properties']['sheetId']
+
+print(f"Created a new subsheet with ID: {new_sheet_id}")
+
